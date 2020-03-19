3d0a63d8d5ff51149b868ffdeef9e3fa2748ab43
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-03-19 14:59:17 +0100

    refactor(Resources, Services): Eliminar todo el codigo de resources y services

--- resources/updateQuality.py
+++ resources/updateQuality.py
@@ -1,9 +1,3 @@
 from flask_restful import Resource, Api
 # from repository.models import Item
 from services.service import Service
-
-
-class UpdateQuality(Resource):
-
-    def get(self):
-        return Service.updateQuality()
