8728cf766e2936294c2cacbbdedd80d958a790db
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-21 17:28:19 +0100

    feat: Añadir clase Sell_in

--- resources/sellin.py
+++ resources/sellin.py
@@ -1,3 +1,7 @@
 from flask_restful import Resource, Api
 # from repository.models import Item
 from services.service import Service
+
+class Sell_in(Resource):
+    def get(self, itemSell_in):
+        return Service.filterSell_in(itemSell_in), 200
