66c45fed08c8ccdd0afe256ca434bed94670e65c
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-21 16:16:23 +0100

    build: Descomentar end's para la filtrar con Quality

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ from resources.inventario import Inventario
 # from resources.updateQuality import UpdateQuality
 from resources.root import Root
 from resources.items import Items
-# from resources.quality import Quality
+from resources.quality import Quality
 # from resources.sellin import SellIn
 
 # from config import *
@@ -30,7 +30,7 @@ api.add_resource(Root, '/')
 api.add_resource(Inventario, '/inventario')
 # api.add_resource(UpdateQuality, '/update-quality')
 api.add_resource(Items, '/items/name/<itemName>', '/items')
-# api.add_resource(Quality, '/items/quality/<itemQuality>')
+api.add_resource(Quality, '/items/quality/<itemQuality>')
 # api.add_resource(SellIn, '/items/sellin/<itemSellIn>')
 
 if __name__ == '__main__':
