7c59658815b6351431b3d435dd184d95fc2bf889
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-21 18:09:26 +0100

    refactor: Cambiar nombre parámetros

--- resources/sellin.py
+++ resources/sellin.py
@@ -3,5 +3,5 @@ from flask_restful import Resource, Api
 from services.service import Service
 
 class SellIn(Resource):
-    def get(self, itemSell_in):
-        return Service.filterSell_in(itemSell_in), 200
+    def get(self, itemSellIn):
+        return Service.filterSell_in(itemSellIn), 200
