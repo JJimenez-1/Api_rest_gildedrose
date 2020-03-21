94bb1c62999a2b2a8f3f626616d12cfa413b9b89
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-21 17:44:14 +0100

    refactor: Cambiar nombre SellIn

--- resources/sellin.py
+++ resources/sellin.py
@@ -2,6 +2,6 @@ from flask_restful import Resource, Api
 # from repository.models import Item
 from services.service import Service
 
-class Sell_in(Resource):
+class SellIn(Resource):
     def get(self, itemSell_in):
         return Service.filterSell_in(itemSell_in), 200
